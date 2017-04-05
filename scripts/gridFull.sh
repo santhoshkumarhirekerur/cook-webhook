eb8d01d5cbb1a5c61de32a0edf076112da0b8d8c
Kumar <skumar@CCC.CookGroup.NAO>
2017-04-05 14:33:27 +1000

    first commit

---
+++ scripts/gridFull.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# My first script
+
+
+
+  cat /etc/webhook/data/db.json | jq '.'
---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# My first script
+
+
+
+if [[ -n "${HOOK_gridName}" ]]; then
+  echo "GRID VALUE : ${HOOK_gridName}"
+fi

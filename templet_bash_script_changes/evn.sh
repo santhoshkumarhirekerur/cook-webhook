a086f0fbb5ff41bccdb970ba6b568cb1f9c793ee
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2017-04-11 13:03:31 +1000

    Autodiscovery using hostname, DEBUG mode & Standard DockerFile

--- templet_bash_script_changes/evn.sh
+++ templet_bash_script_changes/evn.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
+# TODO suggest re-naming this to the actual init-server.sh script for consistency
 
-PS3='Please select your GRID NAME: '
-value="$(curl http://10.61.40.101:9000/hooks/gridInfo)"
-options="$(echo ${value} | jq '.[0].grids[]| .name')"
+# Lookup Conversion
+# Following HOST Naming standards last 2 characters of server name are environmental assignment
+# D = dev
+# P = production
+node_env_tmp=$(echo $HOSTZ | cut -d'-' -f3 | head -c 1)
+case "$node_env_tmp" in
+  D)
+  node_env="dev"
+  ;;
+  P)
+  node_env="prod"
+  ;;
+esac
 
-oldIFS=$IFS
-IFS=$'\n'
-choices=( $options )
-IFS=$oldIFS
-PS3="Please enter your choice: "
-select answer in "${choices[@]}"; do
-  for item in "${choices[@]}"; do
-    if [[ $item == $answer ]]; then
-      break 2
-    fi
+# GridName for Request
+node_appname=$(echo $HOSTZ | cut -d'-' -f2)
+node_gridname="${node_appname,,}-$node_env"
+
+value="$(curl http://localhost:8000/hooks/gridInfo)"
+
+# Just set the global variable DEBUG_COOK=true to activate the code selection
+if [ ! -z $DEBUG_COOK ]; then
+  PS3='Please select your GRID NAME: '
+  options="$(echo ${value} | jq '.[0].grids[]| .name')"
+
+  oldIFS=$IFS
+  IFS=$'\n'
+  choices=( $options )
+  IFS=$oldIFS
+  PS3="Please enter your choice: "
+  select answer in "${choices[@]}"; do
+    for item in "${choices[@]}"; do
+      if [[ $item == $answer ]]; then
+        break 2
+      fi
+    done
   done
-done
-echo $answer
+  answer=$(echo ${answer//\"/})
+else
+  answer=$node_gridname
+fi
 
-answer=$(echo ${answer//\"/})
+# Returning grid information
+# TODO: This should really be moved into the actual service so the service only returns this information for security reasons
+echo $answer
 IP="$(echo ${value} | jq '.[0].masterDNS')"
 
 master_ip=$(echo ${IP//\"/})
@@ -30,6 +57,7 @@ TOKEN="$(echo ${value} | jq --arg ans $answer '.[0].grids[]|select(.name==$ans)|
 grid_token=$(echo ${TOKEN//\"/})
 echo $grid_token
 
-
+# This won't work! this sub-shell doesn't have access to change the parents environmental variables
+# Use SED and directly update the env.sh file or template.yml (whatever makes more sense)
 export MASTER_IP=$master_ip
 export GRID_TOKEN=$grid_token
